6bf53ce72b47497580ebfbad7b5a941ca4187f54
Byron <byronmwong@gmail.com>
2021-07-20 09:21:14 -0700

    add bidirectional page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,107 @@
-import React, { useEffect } from 'react'
-import { defineCustomElements as ionDefineCustomElements } from '@ionic/core/loader'
+import React, { useEffect, useRef, useState } from "react";
+import { defineCustomElements as ionDefineCustomElements } from "@ionic/core/loader";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/core/css/core.css'
+import "@ionic/core/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/core/css/normalize.css'
-import '@ionic/core/css/structure.css'
-import '@ionic/core/css/typography.css'
+import "@ionic/core/css/normalize.css";
+import "@ionic/core/css/structure.css";
+import "@ionic/core/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/core/css/padding.css'
-import '@ionic/core/css/float-elements.css'
-import '@ionic/core/css/text-alignment.css'
-import '@ionic/core/css/text-transformation.css'
-import '@ionic/core/css/flex-utils.css'
-import '@ionic/core/css/display.css'
+import "@ionic/core/css/padding.css";
+import "@ionic/core/css/float-elements.css";
+import "@ionic/core/css/text-alignment.css";
+import "@ionic/core/css/text-transformation.css";
+import "@ionic/core/css/flex-utils.css";
+import "@ionic/core/css/display.css";
 
 function MyApp({ Component, pageProps }) {
+  const [isBackTransition, setIsBackTransition] = useState(false);
+  const currentZIndex = useRef(0);
+  const historyStateIndex = useRef(0);
+
+  if (typeof window !== "undefined") {
+    historyStateIndex.current = history.state.idx;
+  }
+
   useEffect(() => {
-    ionDefineCustomElements(window)
-  })
+    ionDefineCustomElements(window);
+  });
+
+  const router = useRouter();
+  useEffect(() => {
+    router.beforePopState((args) => {
+      const newIndex = (args as any).idx;
+      const currentIndex = historyStateIndex.current;
+      if (currentIndex === undefined || newIndex < currentIndex) {
+        console.log("is a back transition");
+        // subtract 2 because we add 1 on any navigation, resulting in net -1
+        currentZIndex.current -= 2;
+        setIsBackTransition(true);
+      }
+
+      return true;
+    });
+
+    const handleStart = (args) => {
+      currentZIndex.current += 1;
+    };
+    const handleComplete = (...args) => {
+      setIsBackTransition(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+    };
+  }, []);
+
   return (
     <ion-app>
-      <ion-header translucent>
-        <ion-toolbar>
-          <ion-title>Next.js with Ionic</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ion-content fullscreen>
-        <Component {...pageProps} />
-      </ion-content>
-      <ion-footer>
-        <ion-toolbar>
-          <ion-title>Footer</ion-title>
-        </ion-toolbar>
-      </ion-footer>
+      <AnimatePresence initial={false}>
+        <PageAnimator
+          key={router.pathname}
+          {...{
+            Component,
+            pageProps,
+            zIndex: currentZIndex.current,
+            isBackTransition: isBackTransition,
+          }}
+        />
+      </AnimatePresence>
     </ion-app>
-  )
+  );
 }
 
-export default MyApp
+function PageAnimator({ Component, pageProps, zIndex, isBackTransition }) {
+  const [myZIndex] = useState(zIndex);
+
+  return (
+    <motion.div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        background: "white",
+        zIndex: myZIndex,
+      }}
+      initial={isBackTransition ? undefined : { y: 200, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={isBackTransition ? { y: 200, opacity: 0 } : { opacity: 0.999999 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,48 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <ion-grid>
-      <ion-row>
-        {new Array(8).fill('').map((k, i) => (
-          <ion-col key={i} size="3">
-            <ion-card>
-              <Image
-                src="/cat.jpg"
-                alt="Picture of the author"
-                width={500}
-                height={500}
-              />
-              <ion-card-header>
-                <ion-card-subtitle>Destination</ion-card-subtitle>
-                <ion-card-title>Madison, WI</ion-card-title>
-              </ion-card-header>
-              <ion-card-content>
-                <ion-icon name="pin" slot="start"></ion-icon>
-                Keep close to Nature's heart... and break clear away, once in
-                awhile, and climb a mountain or spend a week in the woods. Wash
-                your spirit clean.
-              </ion-card-content>
-            </ion-card>
-          </ion-col>
-        ))}
-      </ion-row>
-    </ion-grid>
-  )
+    <>
+      <ion-header translucent>
+        <ion-toolbar>
+          <ion-title>Home</ion-title>
+          <ion-buttons slot="end">
+            <ion-button>
+              <ion-icon slot="icon-only" name="settings-sharp"></ion-icon>
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content fullscreen>
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <Link href="/customizer" passHref>
+                <ion-card>
+                  <Image
+                    src="/cat.jpg"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                  />
+                  <ion-card-header>
+                    <ion-card-subtitle>Destination</ion-card-subtitle>
+                    <ion-card-title>Madison, WI</ion-card-title>
+                  </ion-card-header>
+                  <ion-card-content>
+                    <ion-icon name="pin" slot="start"></ion-icon>
+                    Keep close to Nature's heart... and break clear away, once
+                    in awhile, and climb a mountain or spend a week in the
+                    woods. Wash your spirit clean.
+                  </ion-card-content>
+                </ion-card>
+              </Link>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-content>
+    </>
+  );
 }
